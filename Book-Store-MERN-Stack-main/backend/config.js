7602db964834ad7526adf3185c73fbeb0a44e77f
diliphackerone <dilip.hackerone.8@gmail.com>
2025-03-29 14:41:21 +0530

    first commit

---
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://diliphackerone8:d9k.ZPBpM!KFwqw@devopsdilip.dudrh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=DevopsDilip';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
+//mongodb+srv://diliphackerone8:d9k.ZPBpM!KFwqw@devopsdilip.dudrh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=DevopsDilip
+//ongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority
