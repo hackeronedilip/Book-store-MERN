604024e0beb6b8fbe4b2b2b0b10b7592cda2d0cd
diliphackerone <dilip.hackerone.8@gmail.com>
2025-03-29 14:52:51 +0530

    added correct Db path

--- Book-Store-MERN-Stack-main/backend/config.js
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -1,9 +1,11 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://diliphackerone8:d9k%2EZPBpM%21KFwqw@devopsdilip.dudrh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=DevopsDilip';
+  'mongodb+srv://admin:admin@devopsdilip.dudrh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=DevopsDilip';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
-//mongodb+srv://diliphackerone8:d9k.ZPBpM!KFwqw@devopsdilip.dudrh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=DevopsDilip
+//mongodb+srv://diliphackerone8:d9k.ZPBpM!KFwqw@devopsdilip.dudrh.mongodb.net/?retryWrites=true&w=majority&appName=DevopsDilip
 //ongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority
+
+// /mongodb+srv://admin:admin@devopsdilip.dudrh.mongodb.net/
