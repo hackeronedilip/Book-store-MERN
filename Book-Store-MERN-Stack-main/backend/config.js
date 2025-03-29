9658b0d534e59c7c517e2d877a3afcc6e6e47791
diliphackerone <dilip.hackerone.8@gmail.com>
2025-03-29 14:44:40 +0530

    added mongo url

--- Book-Store-MERN-Stack-main/backend/config.js
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://diliphackerone8:d9k.ZPBpM!KFwqw@devopsdilip.dudrh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=DevopsDilip';
+  'mongodb+srv://diliphackerone8:d9k%2EZPBpM%21KFwqw@devopsdilip.dudrh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=DevopsDilip';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
